d9a859342fb9bf48cc91a810f4f87f9c3e1db2c4
Nithesh24 <65641597+Nithesh24@users.noreply.github.com>
2022-12-09 18:01:46 +0530

    timer work

--- src/components/Pomo.js
+++ src/components/Pomo.js
@@ -1,9 +1,26 @@
 const Pomo = () =>{
     return(
         <div className = "pomobox">
-            <h1> </h1>
+            <div className = "wrapper">
+                <div className = "timeflex">
+                    <span className = "seconds">00</span>
+                    <span className = "tens">00</span>
+                </div>
+                <div className = "buttonflex">
+                    <button className="btn-start">Start</button>
+                    <button className="btn-reset">Reset</button>
+                </div>
+            </div>
         </div>
     )
+    let seconds = 0;
+    let tens = 0;
+    let getSeconds = document.getElementsByClassName("seconds")
+    let getTens = document.getElementsByClassName("tens")
+    let btnStart = document.getElementsByClassName("btn-start")
+    let btnReset = document.getElementsByClassName("btn-reset")
+
+    console.log(getSeconds.innerHTML)
 }
 
 export default Pomo
